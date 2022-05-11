3c02fb3d7d1711ce3a9e8f0b727ad031433d36e7
LucaGherardini <luca.gherardini96@gmail.com>
2022-05-11 18:40:28 +0200

    improve prefix output and add "threads" option

--- conv_vcf2plink.py
+++ conv_vcf2plink.py
@@ -3,11 +3,12 @@ import sys
 from subprocess import Popen, PIPE, STDOUT
     
 part_number = 1
+threads = int(input('Insert number of threads: ')) if len(sys.argv) < 3 else sys.argv[2]
 
 output = Popen(f"find . -name \'*.vcf\'", shell=True, stdout=PIPE)
 files = str(output.stdout.read()).removeprefix('b\'').removesuffix('\'').removesuffix('\\n').split('\\n')
 print(files)
 for f in files:
-    os.system(f"plink --vcf {f} --make-bed --out p_{part_number}")
+    os.system(f"plink --threads {threads} --vcf {f} --make-bed --out p_{part_number}")
     # NOTE: you can't overwrite directly the input file
     part_number += 1
--- extract_SNPs.py
+++ extract_SNPs.py
@@ -3,9 +3,10 @@ import sys
 from subprocess import Popen, PIPE, STDOUT
 
 stem = input('Insert stem: ') if len(sys.argv) < 2 else sys.argv[1]
-    
+threads = int(input('Insert number of threads: ')) if len(sys.argv) < 3 else sys.argv[2]
+
 output = Popen(f"find . -name \'{stem}*.bed\'", shell=True, stdout=PIPE)
 files = str(output.stdout.read()).replace('b\'', '').replace("\'", '').replace('./', '').replace('.bed', '').removesuffix('\\n').split('\\n')
 print(files)
 for f in files:
-    os.system(f"plink --bfile {f} --snps-only --make-bed --out snp_{f}")
+    os.system(f"plink --threads {threads} --bfile {f} --snps-only --make-bed --out ext_{f}")
